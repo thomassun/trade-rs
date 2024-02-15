fa653aa84c1df6aa2c81ac1f0cfa5120aeb0470c
thomas.sun <sguoqing@gmail.com>
2024-02-15 17:23:49 +0800

    typo

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn main() -> Result<()> {
         [
             "btcusdt@aggTrade",
             "btcusdt@depth",
-            "btcusdt@avgPrice"
+            "btcusdt@avgPrice",
             "ethusdt@avgPrice"
         ],
     "id": 1234
